6164b9f271f719cc06607a64a5b64bd063272c9a
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-12 14:17:04 -0700

    Clean up promises on auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,24 +5,10 @@ module.exports.createSession = (req, res, next) => {
 
   if (req.cookies === undefined || Object.keys(req.cookies).length === 0) {
     models.Sessions.create()
-      .then( result => {
-        models.Sessions.get({id: result.insertId})
-          .then( session => {
-            req.session = session;
-          })
-          .catch( err => {
-            throw err;
-          })
-          .finally( () => {
-            next();
-          });
-      })
-      .catch( err => {
-        console.error(err);
-      })
-      .finally( () => {
-        next();
-      });
+      .then(result => models.Sessions.get({ id: result.insertId }))
+      .then(session => req.session = session)
+      .catch(err => console.error(err))
+      .finally( () => next());
   }
 };
 
