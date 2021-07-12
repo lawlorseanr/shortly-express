3e5690e5c0385e65e2a596e6ef0bf1221a17b3ed
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-12 15:25:09 -0700

    Implement create session if cookies exist, but no session

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,18 +1,43 @@
 const models = require('../models');
 const Promise = require('bluebird');
+const hashUtils = require('../lib/hashUtils');
 
 module.exports.createSession = (req, res, next) => {
 
   if (req.cookies === undefined || Object.keys(req.cookies).length === 0) {
+
     models.Sessions.create()
-      .then(result => models.Sessions.get({ id: result.insertId }))
-      .then(session => req.session = session)
+      .then(response => models.Sessions.get({ id: response.insertId }))
+      .then(session => {
+        req.session = session;
+        res.cookies['shortlyid'] = {value: session.hash};
+      })
       .catch(err => console.error(err))
       .finally( () => next());
+  } else {
+    models.Sessions.get({ hash: req.cookies.shortlyid })
+      .then( session => {
+        if (session) {
+          req.session = session;
+        } else {
+          // TODO clean me up
+          req.cookies = {};
+          models.Sessions.create()
+            .then(response => models.Sessions.get({ id: response.insertId }))
+            .then(session => {
+              req.session = session;
+              res.cookies['shortlyid'] = {value: session.hash};
+            })
+            .catch(err => console.error(err))
+            .finally( () => next());
+        }
+
+      })
+      .catch (err => console.error(err))
+      .finally(() => next());
   }
 };
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
-
